0f7b013d5a3932f421d7715a5fdd675b26a11527
GuillhFreitas <guilherme.antraco@gmail.com>
2023-02-08 16:10:42 +0000

    clean code before merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,12 @@ vidSearch(searchTerm);
 let videoIdArray = [];
 
 function vidSearch(searchTerm){
-
     // number of videos to list, 0 - 50
     const maxResults = 3;
-
     // takes a string, returns only embeddable videos if set to 'true', returns any video if set to 'any'
     const videoEmbeddable = 'any';
-
     // takes a string, returns only videos that can be played outside of youtube if set to 'true', returns any video if set to 'any'
     const videoSyndicated = 'any';
-
     // the youtube API query
     let queryURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${searchTerm}&type=video&videoEmbeddable=${videoEmbeddable}&videoSyndicated=${videoSyndicated}&key=AIzaSyAaHlsRrNz-Id4A5QSxQDmMAAg7Atuz5V0`;
     fetch(queryURL)
@@ -22,15 +18,12 @@ function vidSearch(searchTerm){
         const vidList = response.items;
         console.log(vidList);
         let allVideosEl = document.querySelector('#videos');
-
         // builds and attaches the elements containing the videos
         for (let i = 0; i < vidList.length; i++) {
             const videoId = vidList[i].id.videoId;
             videoIdArray.push(videoId);
-
             const video = vidList[i].snippet;
             let videoEl = allVideosEl.children[i];
-
             // the quick way
             videoEl.innerHTML = `<button type="button" class="btn" data-toggle="modal" data-id="${i}" data-target="#embed">
                 <h6 class='card-title' id='video-title'>${video.title}</h6>
@@ -40,37 +33,6 @@ function vidSearch(searchTerm){
                     <p>by ${video.channelTitle}</p>
                 </div>
                 </button>`
-            
-            // saved the old way for reference
-            // videoEl.innerHTML = '';
-            // let anchorEl = document.createElement('a');
-            // anchorEl.setAttribute('href', `https://youtu.be/${videoId}`);
-
-            // let titleEl = document.createElement('h6');
-            // titleEl.setAttribute('class', 'card-title');
-            // titleEl.textContent = video.title;
-            // anchorEl.appendChild(titleEl);
-
-            // let thumbnailEl = document.createElement('img');
-            // thumbnailEl.setAttribute('src', video.thumbnails.default.url);
-            // thumbnailEl.setAttribute('class', 'card-img-top');
-            // anchorEl.appendChild(thumbnailEl);
-
-            // let divEl = document.createElement('div');
-
-            // let descriptionEl = document.createElement('p');
-            // descriptionEl.textContent = video.description;
-            // divEl.appendChild(descriptionEl);
-
-            // let channelEl = document.createElement('p');
-            // channelEl.textContent = `by ${video.channelTitle}`;
-            // divEl.appendChild(channelEl);
-
-            // anchorEl.appendChild(divEl);
-            
-            // videoEl.appendChild(anchorEl);
-
-            
 
         }
     })
